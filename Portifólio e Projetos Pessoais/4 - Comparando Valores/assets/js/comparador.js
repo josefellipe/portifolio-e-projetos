15ce267619fba12a1b5abb4da8bcb9d8a21202ad
josefellipe <joseserqueira@id.uff.br>
2022-07-05 10:31:29 -0300

    Atualizando codigo comentado do projeto equação cubica

--- Portifólio e Projetos Pessoais/4 - Comparando Valores/assets/js/comparador.js
+++ Portifólio e Projetos Pessoais/4 - Comparando Valores/assets/js/comparador.js
@@ -1,9 +1,12 @@
 function calcular() {
+    //variáveis do input, são os termos da equação cubica
     var termoCub = parseFloat(document.getElementById("termoCubico").value);
     var termoQua = parseFloat(document.getElementById("termoQuadratico").value);
     var termoLin = parseFloat(document.getElementById("termoLinear").value);
     var termoInd = parseFloat(document.getElementById("termoIndependente").value);
+    //variável de saída, é o valor que desejamos encontrar
     var raiz;
+    //variáveis auxiliáres para usar no calculo
     var resultd;
     var resulte;
     var i = 1;
@@ -11,20 +14,25 @@ function calcular() {
     var xe;
     var aux;
 
+    //verificar se a raíz é zero
     if (termoInd == 0) {
         raiz = 0;
     }
     else {
+        //varificar se o primeiro intervalo não é o próprio valor da raíz
         var resultaux = parseFloat(termoCub * i ** 3) + parseFloat(termoQua * i ** 2) + parseFloat(termoLin * i) + parseFloat(termoInd);
         if (resultaux == 0) {
             raiz = i;
         }
         else {
+            //verificar se a raíz está no primeiro intervalo de i=1
             resultd = parseFloat(termoCub * i ** 3) + parseFloat(termoQua * i ** 2) + parseFloat(termoLin * i) + parseFloat(termoInd);
             resulte = parseFloat(termoCub * (-i) ** 3) + parseFloat(termoQua * (-i) ** 2) + parseFloat(termoLin * (-i)) + parseFloat(termoInd);
 
-            while (-resulte * resultd < 0) {
+            while (resulte * resultd > 0) {
+                //enquanto o produto for maior que zero a raíz ainda não está contida no intervalo
                 if (resulte == 0) {
+                    //verificar se a raíz não é o valor do intervalo (primeiro o valor da esquerda)
                     raiz = -i;
                 }
                 else {
@@ -32,44 +40,38 @@ function calcular() {
                         raiz = i;
                     }
                     else {
+                        //aumentando o valor do intervalo e atribuindo os novos resultados dos extremos
                         i = parseFloat(i) + 1;
                         resultd = parseFloat(termoCub * i ** 3) + parseFloat(termoQua * i ** 2) + parseFloat(termoLin * i) + parseFloat(termoInd);
                         resulte = parseFloat(termoCub * (-i) ** 3) + parseFloat(termoQua * (-i) ** 2) + parseFloat(termoLin * (-i)) + parseFloat(termoInd);
                     }
                 }
             }
-
+            //quando encontramos o valor do intervalo que contem a raíz começamos a comparar em qual metade do intervalo a raíz está
             resultaux = termoInd;
             xd = i;
             xe = -i;
             aux = 0;
 
-            if (resultd == 0) {
-                raiz = i;
-            }
-            else {
-                if (resulte == 0) {
-                    raiz = -i;
+            while ((parseFloat(xd) - parseFloat(xe)) ** 2 >= 0.0001) {
+                //enquanto o intervalo for maior que a tolerancia desejada
+                resultd = parseFloat(termoCub * xd ** 3) + parseFloat(termoQua * xd ** 2) + parseFloat(termoLin * xd) + parseFloat(termoInd);
+                resulte = parseFloat(termoCub * xe ** 3) + parseFloat(termoQua * xe ** 2) + parseFloat(termoLin * xe) + parseFloat(termoInd);
+                resultaux = parseFloat(termoCub * aux ** 3) + parseFloat(termoQua * aux ** 2) + parseFloat(termoLin * aux) + parseFloat(termoInd);
+                //procurando em qual metade do intervalo a raíz está e parametrizando um novo valor auxiliar
+                if (resultaux * resultd < 0) {
+                    xe = aux;
+                    aux = (parseFloat(aux) + parseFloat(xd)) / 2;
+                    raiz = xd
                 }
                 else {
-                    while ((parseFloat(xd) - parseFloat(xe)) ** 2 >= 0.0001) {
-                        resultd = parseFloat(termoCub * xd ** 3) + parseFloat(termoQua * xd ** 2) + parseFloat(termoLin * xd) + parseFloat(termoInd);
-                        resulte = parseFloat(termoCub * xe ** 3) + parseFloat(termoQua * xe ** 2) + parseFloat(termoLin * xe) + parseFloat(termoInd);
-                        resultaux = parseFloat(termoCub * aux ** 3) + parseFloat(termoQua * aux ** 2) + parseFloat(termoLin * aux) + parseFloat(termoInd);
-                        if (resultaux * resultd < 0) {
-                            xe = aux;
-                            aux = (parseFloat(aux) + parseFloat(xd)) / 2;
-                            raiz = xd
-                        }
-                        else {
-                            xd = aux;
-                            aux = (parseFloat(aux) + parseFloat(xe)) / 2;
-                            raiz = xd
-                        }
-                    }
+                    xd = aux;
+                    aux = (parseFloat(aux) + parseFloat(xe)) / 2;
+                    raiz = xd
                 }
             }
         }
     }
+    //imprimindo o valor no html de destino com 3 casas decimais
     document.querySelector(".raiz").innerHTML = raiz.toFixed(3);
 }
